16faa5ff9d5f3dcd95f0deffbd1c4fa2fb0d1256
cici cemong <45526554+cicicemong@users.noreply.github.com>
2019-01-13 13:35:46 +0700

    Add files via upload

---
+++ Menentukan program letak kuadran.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int x,y;
+    printf("MENGETAHUI KUADRAN DARI INPUTAN KOORDINAT X DAN Y.\n");
+    printf("Masukan nilai x : ");
+    scanf("%i",&x);
+    printf("Masukan nilai y : ");
+    scanf("%i",&y);
+    if(x>0&&y>0)
+        printf("KUADRAN I");
+    if(x>0&&y<0)
+        printf("KUADRAN II");
+    if(x<0&&y<0)
+        printf("KUADRAN III");
+    if(x<0&&y>0)
+        printf("KUADRAN IV");
+    if(x==0&&y==0)
+        printf("TITIK PUSAT");
+
+    getch();
+}
